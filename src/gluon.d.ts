730b516a20842c3fe36eec69043a7885950ed13f
theusaf <theusafyt@gmail.com>
2024-02-03 09:44:49 -0800

    refactor: add store to gluon types

--- src/gluon.d.ts
+++ src/gluon.d.ts
@@ -23,6 +23,7 @@ declare global {
     send: Gluon.IPCApi["send"];
     on: Gluon.IPCApi["on"];
     removeListener: Gluon.IPCApi["removeListener"];
+    store: Gluon.IPCApi["store"];
   }
 
   interface Window {
