bdd4075a8af6f059c2e54bc80e2ce114ecc6f62c
theusaf <theusafyt@gmail.com>
2024-02-11 11:06:39 -0800

    feat: allow builds for all oses

--- build.sh
+++ build.sh
@@ -1,78 +1,303 @@
 #!/usr/bin/env bash
 
+# Credits to https://www.redhat.com/sysadmin/arguments-options-bash-scripts
+help()
+{
+  # Display Help
+  echo "Builds the application"
+  echo
+  echo "Syntax: ./build.sh [-h|-t <win|mac|linux>|-a <arm64|x64>|-z]"
+  echo "options:"
+  echo "  h   Print this help."
+  echo "  t   Target platform. If not specified, all platforms will be built."
+  echo "        To build for multiple platforms, use this option multiple times."
+  echo "  a   Target architecture. If not specified, all architectures will be built."
+  echo "  z   Compress (zip) the output."
+}
+
+NODE_VERSION="20.11.0"
+
+TARGET_WIN=false
+TARGET_MAC=false
+TARGET_LINUX=false
+ARCH_ARM64=false
+ARCH_X64=false
+COMPRESS_OUTPUT=false
+
+while getopts ":htaz:" option; do
+   case $option in
+      h) # display Help
+        help
+        exit;;
+      t) # Target platform
+        case $OPTARG in
+            win) TARGET_WIN=true;;
+            mac) TARGET_MAC=true;;
+            linux) TARGET_LINUX=true;;
+            *) echo "Error: Invalid target platform"
+              exit;;
+        esac;;
+      a) # Target architecture
+        case $OPTARG in
+            arm64) ARCH_ARM64=true;;
+            x64) ARCH_X64=true;;
+            *) echo "Error: Invalid target architecture"
+              exit;;
+        esac;;
+      z) # Compress output
+        COMPRESS_OUTPUT=true;;
+      \?) # Invalid option
+        echo "Error: Invalid option"
+        exit;;
+   esac
+done
+
+if [[ TARGET_LINUX = false && TARGET_MAC = false && TARGET_WIN = false ]]; then
+  TARGET_LINUX=true
+  TARGET_MAC=true
+  TARGET_WIN=true
+fi
+
+if [[ ARCH_ARM64 = false && ARCH_X64 = false ]]; then
+  ARCH_ARM64=true
+  ARCH_X64=true
+fi
+
 # Download Node.js
 mkdir -p build
 
-if [ ! -f "build/node-arm64.zip" ]; then
-  curl -L https://nodejs.org/dist/v20.11.0/node-v20.11.0-win-arm64.zip -o build/node-arm64.zip
+echo "Downloading Node.js"
+if [[ ARCH_ARM64 ]]; then
+  if [[ ! -f "build/node-win-arm64.zip" && TARGET_WIN ]]; then
+    curl -L https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-win-arm64.zip -o build/node-win-arm64.zip
+  fi
+  if [[ ! -f "build/node-mac-arm64.zip" && TARGET_MAC ]]; then
+    curl -L https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-darwin-arm64.tar.gz -o build/node-mac-arm64.tar.gz
+  fi
+  if [[ ! -f "build/node-linux-arm64.tar.gz" && TARGET_LINUX ]]; then
+    curl -L https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-arm64.tar.xz -o build/node-linux-arm64.tar.xz
+  fi
 fi
-if [ ! -f "build/node-x64.zip" ]; then
-  curl -L https://nodejs.org/dist/v20.11.0/node-v20.11.0-win-x64.zip -o build/node-x64.zip
+if [[ ARCH_X64 ]]; then
+  if [[ ! -f "build/node-win-x64.zip" && TARGET_WIN ]]; then
+    curl -L https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-win-x64.zip -o build/node-win-x64.zip
+  fi
+  if [[ ! -f "build/node-mac-x64.zip" && TARGET_MAC ]]; then
+    curl -L https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-darwin-x64.tar.gz -o build/node-mac-x64.tar.gz
+  fi
+  if [[ ! -f "build/node-linux-x64.tar.gz" && TARGET_LINUX ]]; then
+    curl -L https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.xz -o build/node-linux-x64.tar.xz
+  fi
 fi
 
 # unzip if needed
-
-if [ ! -d "build/node-arm64" ]; then
-  mkdir -p build/node-arm64
-  unzip build/node-arm64.zip -d build/node-arm64
+echo "Extracting Node.js"
+if [[ ARCH_ARM64 ]]; then
+  if [[ ! -d "build/node-win-arm64" && TARGET_WIN ]]; then
+    mkdir -p build/node-win-arm64
+    unzip build/node-win-arm64.zip -d build/node-win-arm64
+  fi
+  if [[ ! -d "build/node-mac-arm64" && TARGET_MAC ]]; then
+    mkdir -p build/node-mac-arm64
+    tar -xvzf build/node-mac-arm64.tar.gz -C build/node-mac-arm64
+  fi
+  if [[ ! -d "build/node-linux-arm64" && TARGET_LINUX ]]; then
+    mkdir -p build/node-linux-arm64
+    tar -xvJf build/node-linux-arm64.tar.xz -C build/node-linux-arm64
+  fi
 fi
-if [ ! -d "build/node-x64" ]; then
-  mkdir -p build/node-x64
-  unzip build/node-x64.zip -d build/node-x64
+if [[ ARCH_X64 ]]; then
+  if [[ ! -d "build/node-win-x64" && TARGET_WIN ]]; then
+    mkdir -p build/node-win-x64
+    unzip build/node-win-x64.zip -d build/node-win-x64
+  fi
+  if [[ ! -d "build/node-mac-x64" && TARGET_MAC ]]; then
+    mkdir -p build/node-mac-x64
+    tar -xvzf build/node-mac-x64.tar.gz -C build/node-mac-x64
+  fi
+  if [[ ! -d "build/node-linux-x64" && TARGET_LINUX ]]; then
+    mkdir -p build/node-linux-x64
+    tar -xvJf build/node-linux-x64.tar.xz -C build/node-linux-x64
+  fi
 fi
 
 # create tmp directory for source files
 mkdir -p build/tmp
 
 # copy source files
+echo "Copying source files"
 cp -r dist/ build/tmp/
-cp -r build-backend/ build/tmp/backend
+cp build-backend/app.js build/tmp/
 cp package.json build/tmp
 
-# install dependencies
-cd build/tmp
-npm install --omit=dev
-cd ../..
-
 # make output directories
-if [ -d "build/arm64" ]; then
-  rm -rf build/arm64
+echo "Recreating output directories"
+if [ -d "build/arm64-win" ]; then
+  rm -rf build/arm64-win
 fi
-if [ -d "build/x64" ]; then
-  rm -rf build/x64
+if [ -d "build/arm64-mac" ]; then
+  rm -rf build/arm64-mac
 fi
-mkdir -p build/arm64/bin
-mkdir -p build/x64/bin
+if [ -d "build/arm64-linux" ]; then
+  rm -rf build/arm64-linux
+fi
+if [ -d "build/x64-win" ]; then
+  rm -rf build/x64-win
+fi
+if [ -d "build/x64-mac" ]; then
+  rm -rf build/x64-mac
+fi
+if [ -d "build/x64-linux" ]; then
+  rm -rf build/x64-linux
+fi
+
+if [[ ARCH_ARM64 ]]; then
+  if [[ TARGET_WIN ]]; then
+    mkdir -p build/arm64-win/bin
+  fi
+  if [[ TARGET_MAC ]]; then
+    mkdir -p build/arm64-mac/bin
+  fi
+  if [[ TARGET_LINUX ]]; then
+    mkdir -p build/arm64-linux/bin
+  fi
+fi
+if [[ ARCH_X64 ]]; then
+  if [[ TARGET_WIN ]]; then
+    mkdir -p build/x64-win/bin
+  fi
+  if [[ TARGET_MAC ]]; then
+    mkdir -p build/x64-mac/bin
+  fi
+  if [[ TARGET_LINUX ]]; then
+    mkdir -p build/x64-linux/bin
+  fi
+fi
 
 # copy tmp to output
 echo "Copying files to output directories"
-cp -r build/tmp/* build/arm64
-cp -r build/tmp/* build/x64
+if [[ ARCH_ARM64 ]]; then
+  if [[ TARGET_WIN ]]; then
+    cp -r build/tmp/* build/arm64-win
+  fi
+  if [[ TARGET_MAC ]]; then
+    cp -r build/tmp/* build/arm64-mac
+  fi
+  if [[ TARGET_LINUX ]]; then
+    cp -r build/tmp/* build/arm64-linux
+  fi
+fi
+if [[ ARCH_X64 ]]; then
+  if [[ TARGET_WIN ]]; then
+    cp -r build/tmp/* build/x64-win
+  fi
+  if [[ TARGET_MAC ]]; then
+    cp -r build/tmp/* build/x64-mac
+  fi
+  if [[ TARGET_LINUX ]]; then
+    cp -r build/tmp/* build/x64-linux
+  fi
+fi
 
 # copy node files
 echo "Copying node files"
-cp -r build/node-arm64/*/* build/arm64/bin
-cp -r build/node-x64/*/* build/x64/bin
+if [[ ARCH_ARM64 ]]; then
+  if [[ TARGET_WIN ]]; then
+    cp -r build/node-win-arm64/*/* build/arm64-win/bin
+  fi
+  if [[ TARGET_MAC ]]; then
+    cp -r build/node-mac-arm64/*/* build/arm64-mac/bin
+  fi
+  if [[ TARGET_LINUX ]]; then
+    cp -r build/node-linux-arm64/*/* build/arm64-linux/bin
+  fi
+fi
+if [[ ARCH_X64 ]]; then
+  if [[ TARGET_WIN ]]; then
+    cp -r build/node-win-x64/*/* build/x64-win/bin
+  fi
+  if [[ TARGET_MAC ]]; then
+    cp -r build/node-mac-x64/*/* build/x64-mac/bin
+  fi
+  if [[ TARGET_LINUX ]]; then
+    cp -r build/node-linux-x64/*/* build/x64-linux/bin
+  fi
+fi
 
 # copy entrypoint
 echo "Copying entrypoint"
-mv build/app-entrypoint-arm64.exe build/arm64/app.exe
-mv build/app-entrypoint-x64.exe build/x64/app.exe
+if [[ ARCH_ARM64 ]]; then
+  if [[ TARGET_WIN ]]; then
+    cp runner/entry.bat build/arm64-win
+    cp runner/entry.ps1 build/arm64-win
+  fi
+  if [[ TARGET_MAC ]]; then
+    cp runner/entry.sh build/arm64-mac
+  fi
+  if [[ TARGET_LINUX ]]; then
+    cp runner/entry.sh build/arm64-linux
+  fi
+fi
+if [[ ARCH_X64 ]]; then
+  if [[ TARGET_WIN ]]; then
+    cp runner/entry.bat build/x64-win
+    cp runner/entry.ps1 build/x64-win
+  fi
+  if [[ TARGET_MAC ]]; then
+    cp runner/entry.sh build/x64-mac
+  fi
+  if [[ TARGET_LINUX ]]; then
+    cp runner/entry.sh build/x64-linux
+  fi
+fi
 
 # remove temporary folder
+echo "Cleaning up temporary files"
 rm -rf build/tmp
 
-# zip (if arg = zip)
-if [ "$1" = "zip" ]; then
+# zip
+if [[ COMPRESS_OUTPUT ]]; then
   echo "Zipping"
   cd build
-  if [ -f "app-arm64.zip" ]; then
-    rm app-arm64.zip
+  if [[ ARCH_ARM64 ]]; then
+    if [[ TARGET_WIN ]]; then
+      if [ -f "arm64-win.zip" ]; then
+        rm arm64-win.zip
+      fi
+      zip -r arm64-win.zip arm64-win
+    fi
+    if [[ TARGET_MAC ]]; then
+      if [ -f "arm64-mac.zip" ]; then
+        rm arm64-mac.zip
+      fi
+      zip -r arm64-mac.zip arm64-mac
+    fi
+    if [[ TARGET_LINUX ]]; then
+      if [ -f "arm64-linux.zip" ]; then
+        rm arm64-linux.zip
+      fi
+      zip -r arm64-linux.zip arm64-linux
+    fi
   fi
-  zip -r app-arm64.zip arm64
-  if [ -f "app-x64.zip" ]; then
-    rm app-x64.zip
+  if [[ ARCH_X64 ]]; then
+    if [[ TARGET_WIN ]]; then
+      if [ -f "x64-win.zip" ]; then
+        rm x64-win.zip
+      fi
+      zip -r x64-win.zip x64-win
+    fi
+    if [[ TARGET_MAC ]]; then
+      if [ -f "x64-mac.zip" ]; then
+        rm x64-mac.zip
+      fi
+      zip -r x64-mac.zip x64-mac
+    fi
+    if [[ TARGET_LINUX ]]; then
+      if [ -f "x64-linux.zip" ]; then
+        rm x64-linux.zip
+      fi
+      zip -r x64-linux.zip x64-linux
+    fi
   fi
-  zip -r app-x64.zip x64
   cd ..
 fi
