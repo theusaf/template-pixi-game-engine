09d8e72190b376e2d91a1d735fed098fc76d8027
theusaf <theusafyt@gmail.com>
2024-02-12 10:08:00 -0800

    feat(RenderLayer): simplify to just setting scale

--- src/game/render_layer.ts
+++ src/game/render_layer.ts
@@ -1,42 +1,14 @@
-import { Application, Container, Graphics, Sprite, Texture } from "pixi.js";
-
-interface RenderLayerOptions {
-  worldWidth: number;
-  worldHeight: number;
-  pixiApp: Application;
-}
+import { Application, Container } from "pixi.js";
 
 class RenderLayer extends Container {
 
-  pixiApp: Application;
-  worldWidth: number;
-  worldHeight: number;
-
-  constructor({
-    pixiApp,
-    worldWidth,
-    worldHeight
-  }: RenderLayerOptions) {
+  constructor(pixiApp: Application, worldWidth: number, worldHeight?: number) {
     super();
-    this.worldWidth = worldWidth;
-    this.worldHeight = worldHeight;
-    this.pixiApp = pixiApp;
-  }
-
-  getScale() {
-    return this.pixiApp.view.width / this.worldWidth;
-  }
-
-  createSprite(texture: Texture) {
-    const sprite = new Sprite(texture);
-    sprite.scale.set(this.getScale());
-    return sprite;
-  }
-
-  createGeometry() {
-    const geometry = new Graphics();
-    geometry.scale.set(this.getScale());
-    return geometry;
+    if (worldHeight) {
+      this.scale.set(pixiApp.view.width / worldWidth, pixiApp.view.height / worldHeight);
+    } else {
+      this.scale.set(pixiApp.view.width / worldWidth);
+    }
   }
 
 }
